89eceb4393fd2152eab89e72fc44f4fef3f1a731
chaehyun <chaehyun282@naver.com>
2021-03-22 21:19:19 +0900

    ui 수정, add js

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//  스크롤 이벤트 
+document.addEventListener('scroll', function () {
+    var currentScrollValue = document.documentElement.scrollTop;
+        console.log('currentScrollValue is ' + currentScrollValue);
+});
